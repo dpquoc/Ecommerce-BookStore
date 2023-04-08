4fb103f0c9b25c5c19b9f77c8f2ac32e7d67f650
Quan <quan.hoang882002@gmail.com>
2023-04-08 10:45:58 +0700

    fix sql

--- my.sql
+++ my.sql
@@ -29,8 +29,8 @@ CREATE TABLE USER (
   username VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
   role ENUM('user', 'admin') DEFAULT 'user',
-  fullname VARCHAR(255) NOT NULL,
-  bday DATE NOT NULL,
+  fullname VARCHAR(255) ,
+  bday DATE ,
   avt_url VARCHAR(255)
 );
 
