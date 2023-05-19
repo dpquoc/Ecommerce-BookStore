ac7f9aecd1178eb16988b2979e99b74f4b0f4a2d
dpquoc <117615204+dpquoc@users.noreply.github.com>
2023-05-19 17:22:41 +0700

    update mailer config

--- server/mailer.php
+++ server/mailer.php
@@ -14,13 +14,13 @@ function sendEmail($to, $subject, $body) {
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                        //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'mycontroldevil@gmail.com';                         //SMTP username (your Gmail address)
-        $mail->Password   = 'ovzuvwhnjmoprzxv';                    //SMTP password (your Gmail password)
+        $mail->Username   = 'your-email@exmaple.com';                         //SMTP username (your Gmail address)
+        $mail->Password   = 'your-password';                    //SMTP password (your Gmail password)
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;          //Enable TLS encryption
         $mail->Port       = 587;                                    //TCP port to connect to
 
         //Recipients
-        $mail->setFrom('mycontroldevil@gmail.com', 'BookStore');          //Set the sender of the email
+        $mail->setFrom('your-email@exmaple.com', 'BookStore');          //Set the sender of the email
         $mail->addAddress($to);
 
         //Content
